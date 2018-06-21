72475f3867e268ec69c7224807507c8a8779d3ca
meghan <meghanwren@yahoo.com>
2018-06-21 15:01:57 -0400

    fixed buttons on create page

--- scripts/express.js
+++ scripts/express.js
@@ -6,19 +6,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/contact-us', (req, res) => {
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('/contact-us.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../contact-us.html'));
 });
 
-app.get('/actual-flashcard', (req, res) => {
+app.get('/actual-flashcard.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../actual-flashcard.html'));
 });
 
-app.get('/careers', (req, res) => {
+app.get('/careers.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../careers.html'));
 });
 
-app.get('/create', (req, res) => {
+app.get('/create.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../create.html'));
 });
 
