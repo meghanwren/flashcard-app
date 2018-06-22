9bb2703d40ca4c1d5d1c775416edbc23e40bf63f
meghan <meghanwren@yahoo.com>
2018-06-22 11:33:16 -0400

    got server working

--- scripts/express.js
+++ scripts/express.js
@@ -2,27 +2,31 @@ var express = require('express');
 var path = require('path');
 const app = express();
 
+app.use(express.static('scripts'));
+app.use(express.static('styles'));
+app.use
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/index/', (req, res) => {
+app.get('/index.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/contact/', (req, res) => {
+app.get('/contact-us.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../contact-us.html'));
 });
 
-app.get('/actual/', (req, res) => {
+app.get('/actual-flashcard.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../actual-flashcard.html'));
 });
 
-app.get('/career/', (req, res) => {
+app.get('/careers.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../careers.html'));
 });
 
-app.get('/create/', (req, res) => {
+app.get('/create.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../create.html'));
 });
 
