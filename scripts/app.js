486e0323901773a3df340520659c48a56316e367
meghan <meghanwren@yahoo.com>
2018-06-25 10:49:01 -0400

    nunjucks and server

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,62 @@
 (function () {
-  //'use strict';
+  'use strict';
+
+  var cards = [];
+  var currentIndex = -1;
+
+  function Card(stackname, front, back) {
+    this.classname = stackname || 'class';
+    this.front = front || 'front';
+    this.back = back || 'back'
+  }
+
+  function Flashcard(classname, front, back){
+    document.querySelector('#class-output').innerHTML = classname;
+    document.querySelector('#front-output').innerHTML = front;
+    document.querySelector('#back-output').innerHTML = back;
+    cards.push(Card(classname, front, back));
+    currentIndex++;
+  }
+
+  function Display(classname, front, back){
+    document.querySelector('#class-output').innerHTML = classname;
+    document.querySelector('#front-output').innerHTML = front;
+    document.querySelector('#back-output').innerHTML = back;
+  }
   
-  function Flashcard () {
+  function AddFlashcard () {
     this.stackName = document.querySelector('#class-name').value;
     this.frontInput = document.querySelector('#front-of-card').value;
     this.backInput = document.querySelector('#back-of-card').value;
   }
 
-  var submitButton = document.querySelector("#submit"); //submit has a default listener
+  var finishButton = document.querySelector("#finish");
+  var addButton = document.querySelector("#add");
+  var nextButton = document.querySelector("#next");
+  var backButton = document.querySelector("#back");
 
-  submitButton.addEventListener('click', (evt) => {
-    var stack = new Flashcard();
-    document.getElementById('class-output').innerHTML = stack.stackName;
-    document.getElementById('front-output').innerHTML = stack.frontInput;
-    document.getElementById('back-output').innerHTML = stack.backInput;
-    //console.log(stack.stackName + ' ' + stack.frontInput + ' ' + stack.backInput);
+  finishButton.addEventListener('click', (evt) => {
+    var stack = new AddFlashcard();
+    Flashcard(stack.stackName, stack.frontInput, stack.backInput);
+    console.log(stack.stackName + ' ' + stack.frontInput + ' ' + stack.backInput);
   });
+
+  addButton.addEventListener('click', (evt) => {
+    var stack = new AddFlashcard();
+    Flashcard(stack.stackName, stack.frontInput, stack.backInput);
+    console.log(stack.stackName + ' ' + stack.frontInput + ' ' + stack.backInput);
+  });
+
+  nextButton.addEventListener('click', (evt) => {
+  ++currentIndex;
+  Display(cards[currentIndex].classname, cards[currentIndex].front, cards[currentIndex].back);
+  });
+
+  backButton.addEventListener('click', (evt) => {
+    --currentIndex;
+    Display(cards[currentIndex].classname, cards[currentIndex].front, cards[currentIndex].back);
+    });
+
 }());
 
 
--- scripts/express.js
+++ scripts/express.js
@@ -1,33 +1,49 @@
 var express = require('express');
 var path = require('path');
+var nunjucks = require('nunjucks');
+const expressNunjucks = require('express-nunjucks');
+var bodyparser = require('body-parser');
+
 const app = express();
 
 app.use(express.static('scripts'));
 app.use(express.static('styles'));
-app.use
+
+nunjucks.configure('templates', {
+  autoescape: true, //autocompletes html tags
+  express: app
+})
+
+app.set('view engine', 'nunjucks');
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ //parser reads data and turns it into objects
+//   extended: false
+// }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.render(path.join(__dirname, '../templates/header-footer.nunjucks'));
 });
 
-app.get('/index.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+app.get('/header-footer.nunjucks', (req, res) => {
+  res.render(path.join(__dirname, '../templates/header-footer.nunjucks'));
 });
 
-app.get('/contact-us.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../contact-us.html'));
+app.get('/contact-us.nunjucks', (req, res) => {
+  res.render(path.join(__dirname, '../templates/contact-us.nunjucks'));
 });
 
-app.get('/actual-flashcard.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../actual-flashcard.html'));
+// app.get('/actual-flashcard.', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../actual-flashcard.html'));
+// });
+
+app.get('/careers.nunjucks', (req, res) => {
+  res.render(path.join(__dirname, '../templates/careers.nunjucks'));
 });
 
-app.get('/careers.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../careers.html'));
+app.get('/create.nunjucks', (req, res) => {
+  res.render(path.join(__dirname, '../templates/create.nunjucks'));
 });
 
-app.get('/create.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../create.html'));
-});
+
 
 app.listen(5090);
